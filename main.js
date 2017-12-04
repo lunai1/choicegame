47f9e5747f62e8c978480e78b5b97e84c11c8301
Evan Leri <m.evanleri@gmail.com>
2017-12-04 16:57:26 -0500

    ELMBH

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ function first_button() {
     var name = document.getElementById("name_select");
     window.location.href = 'introduction.html';
 }
-
-document.getElementById('say_name').innerHTML = "Your name from this point on is " + name + ". Please put on your Moniter.";
+window.onload = function() {
+  document.getElementById('say_name').innerHTML = "Your name from this point on is " + name + ". Please put on your Moniter.";
+}
