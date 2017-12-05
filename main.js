fd10a43784d5345fbeb50b8bacdfd0ad5e9e6953
Evan Leri <m.evanleri@gmail.com>
2017-12-05 14:48:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-var name;
-function first_button() {
-    name = document.getElementById("name_select");
-    window.location.href = 'introduction.html';
+var name
+var text = document.getElementById('story-container');
+var button = document.getElementById('button-container');
+function pre() {
+    text.innerHTML = 'WELCOME TO GLASS COFFIN.<br>This is a choice led game, so choose wisely.<br> <input type="text" id="name_select"  placeholder="Choose Your Name" />';
+    button.innerHTML = "<button id='name_submit' onclick='start()'>";
 }
-window.onload = function() {
-  document.getElementById('say_name').innerHTML = "Your name from this point on is " + name + ". Please put on your Moniter.";
+function start() {
+    text.innerHTML = "<h1>You wake up in a glass coffin.</h1><h3>You can\'t remember your name.</h3><h3>You have no idea who you are, how you got here, or what is happening</h3><h3>The coffin has opaque sides, but the front is transparent glass, and as you look around,<br>you realize that there are others looking around, or hitting the glass, or sitting with a dazed look.</h3><h2>Suddenly the front panel of all the coffins crash down, and guards swarm you, dragging you into a line of people who have been in the coffins.</h2><h2>Do stay in the line or try to escape?</h2>";
+    button.innerHTML = "<button class='choice_button' onclick='c1()'>STAY</button><button class='choice_button' onclick='c2()'>ESCAPE</button>";
 }
