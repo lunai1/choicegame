b8240c42011fb5dca72dc8ac36ed02a2fcaea8d6
Evan Leri <m.evanleri@gmail.com>
2017-12-07 08:31:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function pre() {
     button.innerHTML = "<button id='name_submit' onclick='start()'>START</button>";
 }
 function start() {
-    name = document.getElementById('name_submit').value;
+    name = document.getElementById('name_select').value;
     text.innerHTML = "<h1>You wake up in a glass coffin.</h1><h3>You can\'t remember your name.</h3><h3>You have no idea who you are, how you got here, or what is happening</h3><h3>The coffin has opaque sides, but the front is transparent glass, and as you look around,<br>you realize that there are others looking around, or hitting the glass, or sitting with a dazed look.</h3><h2>Suddenly the front panel of all the coffins crash down, and guards swarm you, dragging you into a line of people who have been in the coffins.</h2><h2>Do stay in the line or try to escape?</h2>";
     button.innerHTML = "<button class='choice_button' onclick='c1()'>STAY</button><button class='choice_button' onclick='c2()'>ESCAPE</button>";
 }
